0779edb3819d7c8aeed643fe4f5b3f9faca10d96
Guillermo Amante <gamante@moval.es>
2025-02-24 08:46:55 +0100

    [UPD] eom_authdnie: Added translations

--- eom_authdnie/models/res_eom_config_settings.py
+++ eom_authdnie/models/res_eom_config_settings.py
@@ -2,7 +2,7 @@
 # 2024 Moval Agroingeniería
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import models, fields, api
+from odoo import models, fields
 
 
 class ResEomConfigSettings(models.TransientModel):
