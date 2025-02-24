51394cd62f06fe0915902014a56423cb0b582578
Miguel Ángel Rodríguez <marodriguez@moval.es>
2025-02-24 13:50:52 +0100

    [UPD] cim_complaints_channel: Add new changes for migration: - Change use of date fields from db. In v16 this fields are datatime type. - Browse methods do not accept ids as strings. - Changes in views to adapt correctly to the screen size. - Add asset in manyfest field. - Add config_parameter like new parameter in res.config.setting model fields - Change the report type records in the xml files to record type records. - Changes in .less field. Move to .scss field and change some classes

--- cim_complaints_channel/models/res_config_settings.py
+++ cim_complaints_channel/models/res_config_settings.py
@@ -11,12 +11,14 @@ class ResConfigSettings(models.TransientModel):
     sequence_complaint_code_id = fields.Many2one(
         string='Sequence for the codes of complaints',
         comodel_name='ir.sequence',
+        config_parameter="cim_complaints_channel.sequence_complaint_code_id",
         help='Default values of the complaint codes',)
 
     length_tracking_code = fields.Integer(
         string='Length of the tracking code (number of characters)',
         default=10,
         required=True,
+        config_parameter="cim_complaints_channel.length_tracking_code",
         help='Tracking code length to identify complainants '
              '(number of characters)',)
 
@@ -24,6 +26,7 @@ class ResConfigSettings(models.TransientModel):
         string='Acknowledgement period for the complaint admission',
         default=7,
         required=True,
+        config_parameter="cim_complaints_channel.acknowledgement_period",
         help='Acknowledgement period for the complaint admission '
              '(number of days)',)
 
@@ -31,6 +34,7 @@ class ResConfigSettings(models.TransientModel):
         string='E-mail to complainant after change state of complaint (y/n)',
         default=False,
         required=True,
+        config_parameter="cim_complaints_channel.automatic_email_state",
         help='Send e-mail to complainant after change the state of the '
              'complaint',)
 
@@ -38,34 +42,41 @@ class ResConfigSettings(models.TransientModel):
         string='E-mail to complainant after validate communication (y/n)',
         default=False,
         required=True,
+        config_parameter="cim_complaints_channel.automatic_email_validate_com",
         help='Send e-mail to complainant after validate the communication',)
 
     automatic_email_complainant_com = fields.Boolean(
         string='Send the complainant a copy of your communications (y/n)',
         default=False,
         required=True,
+        config_parameter="cim_complaints_channel."
+                         "automatic_email_complainant_com",
         help='Send e-mail to complainant a copy of your communications',)
 
     notice_period = fields.Integer(
         string='Notice Period (number of days)',
         default=10,
         required=True,
+        config_parameter="cim_complaints_channel.notice_period",
         help='Notice period before the complaint deadline (number of days)',)
 
     deadline = fields.Integer(
         string='Deadline (number of months)',
         default=1,
         required=True,
+        config_parameter="cim_complaints_channel.deadline",
         help='Complaint deadline (number of months)',)
 
     deadline_extended = fields.Integer(
         string='Extended Deadline (number of months)',
         default=1,
         required=True,
+        config_parameter="cim_complaints_channel.deadline_extended",
         help='Extended complaint deadline (number of months)',)
 
     email_for_notice = fields.Char(
-        string='E-mail for notice',)
+        string='E-mail for notice',
+        config_parameter="cim_complaints_channel.email_for_notice",)
 
     _sql_constraints = [
         ('valid_length_tracking_code',
