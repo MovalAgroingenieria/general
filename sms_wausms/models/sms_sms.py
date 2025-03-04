ac5dee5f12f38a45934b172a5a234d8025b3a020
Juanu Sandoval <jsandoval@moval.es>
2025-03-04 17:30:10 +0100

    [UPD] sms_wausms: Apply code quality rules

--- sms_wausms/models/sms_sms.py
+++ sms_wausms/models/sms_sms.py
@@ -1,7 +1,7 @@
 # 2025 Moval Agroingeniería
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models, tools, _
+from odoo import fields, models, _
 
 
 class SmsSms(models.Model):
