7ccdc889f4ccce49b24cb2e824902241968bf2e4
Juanu Sandoval <jsandoval@moval.es>
2025-04-16 12:48:29 +0200

    [NEW] transform_database_in_demo: Module creation

---
+++ transform_database_in_demo/__manifest__.py
@@ -0,0 +1,26 @@
+# 2025 Moval Agroingeniería
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+{
+
+    'name': 'Transform database in demo',
+    'summary': 'DANGER: it transforms all records in demo records',
+    'version': '16.0.1.0.0',
+    'category': 'Moval General Addons',
+    'website': 'https://www.moval.es',
+    'author': 'Moval Agroingeniería',
+    'license': 'AGPL-3',
+    'application': False,
+    'installable': True,
+    'post_init_hook': 'post_init_hook',
+    'depends': [
+    ],
+    'data': [
+    ],
+
+}
+
+
+
+
+
