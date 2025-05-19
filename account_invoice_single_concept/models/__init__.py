6f75bf688ff54bac926087c8f899a3aff3742106
Juanu Sandoval <jsandoval@moval.es>
2025-05-19 16:49:21 +0200

    [NEW] account_invoice_single_concept: Module creation (*) Rename and modified module account_invoice_fixed_concept

--- account_invoice_single_concept/models/__init__.py
+++ account_invoice_single_concept/models/__init__.py
@@ -1,5 +1,5 @@
 # 2025 Moval Agroingeniería
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
-from . import account_move_fixed_concept
+from . import account_move_single_concept
 from . import account_move
