b3c84112b3489dcbb963f61f27f51d4966cc73c2
Juanu Sandoval <jsandoval@moval.es>
2025-05-26 12:45:33 +0200

    [NEW] bundle_advanced_accounting_modules: Module creation. Add advanced accouting modules * bundle_common_accounting _modules * analytic * mis_builder * mis_builder_budget * payment

---
+++ bundle_advanced_accounting_modules/__manifest__.py
@@ -0,0 +1,22 @@
+# 2025 Moval Agroingeniería
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+{
+    "name": "Bundle advanced accounting modules",
+    "summary": "Install at once all modules related to the accounting.",
+    "version": "16.0.1.0.0",
+    "author": "Moval Agroingeniería",
+    "license": "AGPL-3",
+    "category": "Hidden",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "bundle_common_accounting _modules",
+        "analytic",
+        "mis_builder",
+        "mis_builder_budget",
+        "payment",
+    ],
+    "data": [
+    ],
+}
