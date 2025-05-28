db87e6039152cb46f01b6ab4464b6a64dbc83b92
Miguel Ángel Rodríguez <marodriguez@moval.es>
2025-05-28 11:43:22 +0200

    [UPD] mrp_mobile_view: Creation module to adapt MRP view form to small devices (phones, tablets,...) and create new button to create and link lot in stock.move.lines

---
+++ mrp_mobile_view/__manifest__.py
@@ -0,0 +1,21 @@
+# 2025 Moval Agroingeniería
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "MRP Mobile View",
+    'version': '16.0.1.0.0',
+    'website': 'http://www.moval.es',
+    "author": "Moval Agroingeniería",
+    "category": "Moval General Addons",
+    'license': 'AGPL-3',
+    'application': False,
+    'installable': True,
+    "depends": [
+        "mrp",
+    ],
+    "data": [
+        "views/mrp_production_view.xml",
+    ],
+    "license": "AGPL-3",
+    'installable': True,
+}
