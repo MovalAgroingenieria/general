8b618724d4f775f9da46bb8fda6d3a6d251522c8
Salvador Sanchez Aroca <ssanchez@moval.es>
2025-07-08 08:59:48 +0200

    [ADD] mrp_production_deconstruction_ext: Implement MRP Production Deconstruction module with BOM and stock move line enhancements.

---
+++ mrp_production_deconstruction_ext/models/mrp_production.py
@@ -0,0 +1,84 @@
+# Copyright 2022 Berezi Amubieta - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo import api, models, fields
+
+
+class MrpProduction(models.Model):
+    _inherit = 'mrp.production'
+
+    is_deconstruction = fields.Boolean(
+        string='Is Deconstruction?',
+        related='bom_id.is_deconstruction',
+        store=True,
+    )
+
+    move_line_ids = fields.One2many(
+        string='Move Lines',
+        comodel_name='stock.move.line',
+        inverse_name='production_id',
+        domain=lambda self: [
+            ('location_dest_id', 'in', self.production_location_id.ids)],
+    )
+
+    finished_move_line_ids = fields.One2many(
+        compute=False,
+        inverse_name='production_id',
+        domain=lambda self: [
+            ('location_id', 'in', self.production_location_id.ids),
+            ('location_dest_id', 'in', self.location_dest_id.ids)],
+    )
+
+    def write(self, vals):
+        res = super(MrpProduction, self).write(vals)
+        if 'move_finished_ids' in vals:
+            for line in self:
+                if line.is_deconstruction and line.move_finished_ids:
+                    for move in line.move_finished_ids:
+                        move.location_id = line.production_location_id.id
+                        move.location_dest_id = line.location_src_id.id
+        if 'move_raw_ids' in vals:
+            for line in self:
+                if line.is_deconstruction and line.move_raw_ids:
+                    for move in line.move_raw_ids:
+                        move.location_id = line.location_src_id.id
+                        move.location_dest_id = line.production_location_id.id
+        return res
+
+    @api.constrains('bom_id', 'is_deconstruction')
+    def _check_is_deconstruction(self):
+        for production in self:
+            if production.bom_id and (
+                production.is_deconstruction) is True and (
+                    production.move_raw_ids):
+                origin = production.picking_type_id.default_location_src_id
+                if origin.usage != 'internal' and (
+                    production.production_location_id.usage) == (
+                        'internal'):
+                    origin = production.production_location_id
+                dest = production.move_raw_ids.location_dest_id
+                if dest.usage == 'production':
+                    old_qtys = {x.id: x.product_uom_qty for x in
+                                   production.move_raw_ids}
+                    # When this happens, the location_src_id is used
+                    # on move raw quantity calculus, so store first and then
+                    # update the quantity (Crappy)
+                    production.write({
+                        'location_dest_id': dest.id,
+                        'location_src_id': dest.id,
+                        'production_location_id': origin.id})
+                    for line in production.move_raw_ids:
+                        line.write({
+                            'location_id': dest.id,
+                            'location_dest_id': origin.id,
+                            'product_uom_qty': old_qtys[line.id]})
+
+    def button_mark_done(self):
+        result = super(MrpProduction, self).button_mark_done()
+        for production in self:
+            if production.is_deconstruction:
+                origin = production.finished_move_line_ids.location_id
+                dest = production.finished_move_line_ids.location_dest_id
+                production.finished_move_line_ids.write({
+                    'location_id': dest.id,
+                    'location_dest_id': origin.id})
+        return result
