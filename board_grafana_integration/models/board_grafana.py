b17a312fa47c68c8a94e13b84e7d07f33b25f7a1
Juanu Sandoval <jsandoval@moval.es>
2025-07-24 13:42:26 +0200

    [UPD] board_grafana: Move config from Board view to own view. Delete integration icon

--- board_grafana_integration/models/board_grafana.py
+++ board_grafana_integration/models/board_grafana.py
@@ -2,7 +2,7 @@
 # 2023 Moval Agroingeniería
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import api, models, fields, exceptions, _
+from odoo import models, fields, exceptions, _
 
 
 class BoardGrafana(models.Model):
