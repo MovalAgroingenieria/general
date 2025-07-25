568eb1bce8705def43da3bc827c6ea8004bb5392
Juanu Sandoval <jsandoval@moval.es>
2025-07-25 13:45:16 +0200

    [UPD] hr_attendance_selfedit: Fix errors caused by permissions in v16

--- hr_attendance_selfedit/__init__.py
+++ hr_attendance_selfedit/__init__.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# 2025 Moval Agroingeniería
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
 from . import models
--- hr_attendance_selfedit/models/hr_attendance.py
+++ hr_attendance_selfedit/models/hr_attendance.py
@@ -1,5 +1,4 @@
-# -*- coding: utf-8 -*-
-# 2022 Moval Agroingeniería
+# 2025 Moval Agroingeniería
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
 import re
@@ -12,105 +11,83 @@ class HrAttendance(models.Model):
 
     attendance_observations = fields.Text(
         string="Observations",
-        help="This field is required if attendance is modified or it is "
-             "created outside the current time (margin 30 minutes) by user. "
-             "Min. 6 chars.",)
+        help="This field is required if attendance is modified or it is created outside the current time"
+             "(margin 30 minutes) by user. Min. 6 chars.",
+    )
 
     attendance_edited = fields.Boolean(
         string="Edited",
         default=False,
-        readonly=True)
+        readonly=True,
+    )
 
     attendance_modificated = fields.Boolean(
         string="Modified",
-        default=False)
+        default=False,
+    )
 
-    @api.depends('attendance_edited')
+    @api.depends("attendance_edited")
     def _compute_attendance_edited_show(self):
         for record in self:
             if record.attendance_edited:
                 record.attendance_edited_show = True
 
-    @api.onchange('check_in', 'check_out')
+    @api.onchange("check_in", "check_out")
     def onchange_checkin_checkout(self):
         for record in self:
-            att_admin = record.env.user.has_group(
-                'hr_attendance.group_hr_attendance_manager')
-            current_att = record.env['hr.attendance'].browse(record._origin.id)
+            att_admin = record.env.user.has_group("hr_attendance.group_hr_attendance_manager")
+            current_att = record.env["hr.attendance"].browse(record._origin.id)
             current_time = datetime.now()
             time_limit_down = current_time - timedelta(minutes=30)
             time_limit_up = current_time + timedelta(minutes=30)
-            # Detect if attendance is modified and edited or is
-            # created outside the current time range
+            # Detect if attendance is modified and edited or is created outside the current time range
             attendance_modificated = False
             if current_att.id and record.check_in:
                 if current_att.check_in != record.check_in:
-                    current_att.write({'attendance_edited': True})
+                    current_att.sudo().write({"attendance_edited": True})
                 if current_att.check_in != record.check_in and not att_admin:
                     attendance_modificated = True
                     record.attendance_modificated = attendance_modificated
-                    if (record.check_in < time_limit_down or
-                            record.check_in > time_limit_up) and not att_admin:
+                    if (record.check_in < time_limit_down or record.check_in > time_limit_up) and not att_admin:
                         record.attendance_modificated = True
                     else:
                         record.attendance_modificated = False
             if current_att.id and record.check_out and current_att.check_out:
                 if current_att.check_out != record.check_out:
-                    current_att.write({'attendance_edited': True})
+                    current_att.sudo().write({"attendance_edited": True})
                 if current_att.check_out != record.check_out and not att_admin:
                     attendance_modificated = True
                     record.attendance_modificated = attendance_modificated
-            if (current_att.id and record.check_out and
-                    current_att.check_out != record.check_out):
-                if (record.check_out < time_limit_down or
-                        record.check_out > time_limit_up) and not att_admin:
+            if (current_att.id and record.check_out and current_att.check_out != record.check_out):
+                if (record.check_out < time_limit_down or record.check_out > time_limit_up) and not att_admin:
                     record.attendance_modificated = True
                 else:
                     record.attendance_modificated = False
 
-    @api.constrains('attendance_observations')
+    @api.constrains("attendance_observations")
     def _check_attendance_observations_length(self):
         for record in self:
-            att_admin = record.env.user.has_group(
-                'hr_attendance.group_hr_attendance_manager')
+            att_admin = record.env.user.has_group("hr_attendance.group_hr_attendance_manager")
             if (record.attendance_modificated and
                     not record.attendance_observations and not att_admin):
-                raise exceptions.ValidationError(
-                    _('The field observations must be filled.'))
+                raise exceptions.ValidationError(_("The field observations must be filled."))
             if record.attendance_observations:
-                att_obs = record.attendance_observations.replace(' ', '')
+                att_obs = record.attendance_observations.replace(" ", "")
                 if (len(att_obs) < 6 or
-                        len(record.attendance_observations) < 6) and not \
-                        att_admin:
-                    raise exceptions.ValidationError(
-                        _('The length of observations must be at least 6 '
-                          'characters.'))
+                        len(record.attendance_observations) < 6) and not att_admin:
+                    raise exceptions.ValidationError(_("The length of observations must be at least 6 characters."))
 
     @api.model
     def create(self, vals):
-        if ('attendance_observations' in vals and
-                vals['attendance_observations']):
-            att_obs = vals['attendance_observations'].lstrip().rstrip()
+        if ("attendance_observations" in vals and vals["attendance_observations"]):
+            att_obs = vals["attendance_observations"].lstrip().rstrip()
             att_obs = re.sub(' +', ' ', att_obs)
             vals['attendance_observations'] = att_obs
         return super(HrAttendance, self).create(vals)
 
     def write(self, vals):
-        for record in self:
-            if ('attendance_observations' in vals and
-                    vals['attendance_observations']):
-                att_obs = vals['attendance_observations'].lstrip().rstrip()
-                att_obs = re.sub(' +', ' ', att_obs)
-                vals['attendance_observations'] = att_obs
-        resp = super(HrAttendance, self).write(vals)
-        return resp
-
-    # Only useful when normal users have unlink permission
-    # def unlink(self):
-    #     for record in self:
-    #         att_admin = record.env.user.has_group(
-    #             'hr_attendance.group_hr_attendance_manager')
-    #         if record.attendance_edited and not att_admin:
-    #             raise exceptions.ValidationError(
-    #                 _('An edited attendance cannot be deleted.'))
-    #     return super(HrAttendance, self).unlink()
+        if ("attendance_observations" in vals and vals["attendance_observations"]):
+            att_obs = vals["attendance_observations"].lstrip().rstrip()
+            att_obs = re.sub(' +', ' ', att_obs)
+            vals["attendance_observations"] = att_obs
+        return super(HrAttendance, self.sudo()).write(vals)
