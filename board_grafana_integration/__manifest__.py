9d3ff254b08b6081fff711817755601faecfd04e
Juanu Sandoval <jsandoval@moval.es>
2025-07-30 20:48:17 +0200

    [UPD] board_grafana_integration: Change order of xml in manifest

--- board_grafana_integration/__manifest__.py
+++ board_grafana_integration/__manifest__.py
@@ -17,8 +17,8 @@
     'data': [
         'security/ir.model.access.csv',
         'views/res_config_settings_view.xml',
-        'views/board_grafana_view.xml',
         'views/base_menu.xml',
+        'views/board_grafana_view.xml',
     ],
     'assets': {
         'web.assets_backend': [
