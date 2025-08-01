0036d5d5bc09a8be2ac38a76d89bd46370d17140
jmartinez <jmartinez@moval.es>
2025-08-01 09:30:44 +0200

    [UPD] project_type_sequence: modified project map function

--- project_type_sequence/models/project_project.py
+++ project_type_sequence/models/project_project.py
@@ -14,11 +14,17 @@ class ProjectProject(models.Model):
                                     offset=offset, limit=limit,
                                     orderby=orderby, lazy=lazy)
         if 'type_id' in groupby and groups:
-            type_ids = [g['type_id'][0] for g in groups if g.get('type_id')]
+            type_ids = [
+                g['type_id'][0] for g in groups
+                if g.get('type_id') and g['type_id']
+            ]
             types = self.env['project.type'].browse(type_ids)
             seq_map = {t.id: t.sequence for t in types}
             groups = sorted(
                 groups,
-                key=lambda g: seq_map.get(g['type_id'][0], 0)
+                key=lambda g: (
+                    seq_map.get(g['type_id'][0], 0)
+                    if g.get('type_id') and g['type_id'] else 0
+                )
             )
         return groups
