e189b1589b4f294ff78e816421008e47e19835b9
jmartinez <jmartinez@moval.es>
2025-08-20 08:31:09 +0200

    [UPD] fleet_vehicle_odometer: added name to model

--- fleet_vehicle_odometer/models/fleet_vehicle.py
+++ fleet_vehicle_odometer/models/fleet_vehicle.py
@@ -12,6 +12,7 @@ from odoo.osv import expression
 class FleetVehicleOdometer(models.Model):
     _inherit = ['mail.thread', 'fleet.vehicle.odometer']
     _description = 'Odometer log for a vehicle'
+    _name = 'fleet.vehicle.odometer'
 
     initial_value = fields.Float(
         string='Initial Odometer Value',
