286cbf0af218a63433306e7875d3cdd9d0133437
jmartinez <jmartinez@moval.es>
2025-09-11 15:15:00 +0200

    [UPD] account_analytic_distribution_ungrouped: fixed the field on more models

--- account_analytic_distribution_ungrouped/__manifest__.py
+++ account_analytic_distribution_ungrouped/__manifest__.py
@@ -1,4 +1,4 @@
-# 2023 Moval Agroingeniería
+# 2025 Moval Agroingeniería
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
@@ -13,17 +13,38 @@
 
         Currently supported:
         - Account Move Lines (invoice lines, journal entries)
+        - Account Bank Statement Lines (reconciliation)
+        - Sale Orders (analytic_account_id field)
+        - Sale Order Lines (analytic_distribution field)
+        - Purchase Order Lines (analytic_distribution field)
+        - Project Projects (analytic_account_id field)
+        - Project Tasks (analytic_account_id field)
+        - Account Analytic Lines (account_id field)
+        - Contract Lines (analytic_distribution field) - recurring invoices
+        - HR Expenses (analytic_distribution field)
+        - Account Assets (analytic_distribution field) - asset management
     """,
     'author': 'Moval',
     'website': 'https://www.moval.es',
     'license': 'AGPL-3',
-    'depends': ['account', 'analytic', 'account_reconcile_oca'],
+    'depends': [
+        'account', 'analytic', 'account_reconcile_oca', 'sale',
+        'purchase', 'project', 'contract', 'hr_expense',
+        'account_asset_management'
+    ],
     'auto_install': False,
     'application': False,
     'sequence': 1000,
     'data': [
         'views/account_move_views.xml',
         'views/account_bank_statement_line_views.xml',
+        'views/sale_order_views.xml',
+        'views/purchase_order_views.xml',
+        'views/project_views.xml',
+        'views/account_analytic_views.xml',
+        'views/contract_views.xml',
+        'views/hr_expense_views.xml',
+        'views/account_asset_views.xml',
     ],
     'installable': True,
     'auto_install': False,
--- account_analytic_distribution_ungrouped/models/__init__.py
+++ account_analytic_distribution_ungrouped/models/__init__.py
@@ -3,3 +3,8 @@
 
 from . import account_move
 from . import account_bank_statement_line
+from . import sale_order_line
+from . import purchase_order
+from . import contract_line
+from . import hr_expense
+from . import account_asset
