47d5ed58c7dfb711214b33560f011b6f8dc5d53a
jmartinez <jmartinez@moval.es>
2025-10-07 15:08:36 +0200

    [UPD] appointment_google_meet_integration: fixed manifest

--- appointment_google_meet_integration/__manifest__.py
+++ appointment_google_meet_integration/__manifest__.py
@@ -1,10 +1,17 @@
 # 2025 Moval Agroingeniería
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
 {
+    'name': 'Appointment Google Meet Integration',
+    'summary': 'Integration between appointments and Google Meet',
+    'version': '16.0.1.0.0',
     'author': 'Moval Agroingeniería',
     'website': 'https://moval.es',
     'license': 'AGPL-3',
+    'category': 'Website/Website',
+    'application': False,
+    'installable': True,
+    'auto_install': False,
     'depends': [
         'base',
         'calendar',
@@ -27,7 +34,4 @@
         'views/calendar_event_views.xml',
         'views/website_templates.xml',
     ],
-    'installable': True,
-    'application': False,
-    'auto_install': False,
 }
