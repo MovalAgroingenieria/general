93a735339c5b3332e0b0ae428c4bcc14f37bf246
jmartinez <jmartinez@moval.es>
2025-10-10 09:14:30 +0200

    [FIX] account_reconcile_partials_fix: fix on manifest

--- account_reconcile_partials_fix/__manifest__.py
+++ account_reconcile_partials_fix/__manifest__.py
@@ -14,20 +14,3 @@
     'data': [
     ],
 }
-
-# 2023 Moval Agroingeniería
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
-{
-    'name': 'Base Report',
-    'summary': 'Common modifications to all reports',
-
-    'depends': [
-        'web',
-    ],
-    'data': [
-        'views/report_templates.xml',
-    ],
-    'assets': {
-    },
-}
