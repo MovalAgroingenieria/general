a69c7438ebd5030e0997bddd5866172ac122360f
Guillermo Amante <gamante@moval.es>
2025-10-14 13:41:00 +0200

    [NEW] remotecontrol_cesens: api integration

---
+++ remotecontrol_cesens/hooks.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# 2020 Moval Agroingeniería
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import api, SUPERUSER_ID
+
+
+def uninstall_hook(cr, registry):
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    env.ref('remotecontrol_cesens.remotecontrol_cesens').with_context(
+        force_unlink=True).unlink()
