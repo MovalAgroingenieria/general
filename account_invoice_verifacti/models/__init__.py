3e460182863eabdb5bf5574028e94e0b4558cba9
jmartinez <jmartinez@moval.es>
2025-10-21 10:54:38 +0200

    [FIX] account_invoice_verifacti: removed account_move model

--- account_invoice_verifacti/models/__init__.py
+++ account_invoice_verifacti/models/__init__.py
@@ -2,12 +2,11 @@
 # 2025 Moval Agroingeniería
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from . import res_company
-from . import res_config_settings
+from . import account_invoice
 from . import account_journal
 from . import account_tax
-from . import verifacti_config
-from . import verifacti_chain
-from . import verifacti_log
-from . import account_invoice
+from . import res_company
+from . import res_config_settings
 from . import verifacti_api_client
+from . import verifacti_config
+from . import verifacti_log
