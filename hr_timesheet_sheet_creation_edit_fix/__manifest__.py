56974ff41f63240f7bfbebe9a4fef9a916f525c3
Miguel Ángel Rodríguez <marodrguez@moval.es>
2025-11-03 09:59:37 +0100

    [NEW] hr_timesheet_sheet_creation_edit_fix: Add module to enable creation and editing of timesheets

---
+++ hr_timesheet_sheet_creation_edit_fix/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'HR Timesheet Sheet Creation and Edit Fix',
+    'version': '16.0.1.0.0',
+    'category': 'Human Resources',
+    'website': 'https://www.moval.es',
+    'author': 'Moval Agroingeniería',
+    'license': 'AGPL-3',
+    'application': False,
+    'installable': True,
+
+    'depends': [
+        'hr_timesheet_sheet',
+    ],
+    'data': [
+    ],
+}
