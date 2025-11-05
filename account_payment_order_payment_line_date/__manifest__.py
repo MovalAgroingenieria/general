2afcb58fa4301b7175c878c67666337cafd15b9d
César Andrés Sánchez <rasec.andres@gmail.com>
2025-11-05 22:42:12 +0100

    [IMP] migrate account_payment_order_payment_line_date pylint

--- account_payment_order_payment_line_date/__manifest__.py
+++ account_payment_order_payment_line_date/__manifest__.py
@@ -2,20 +2,18 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'Account Payment Order: Payment line date',
-    'summary': 'Add payment request date to payment lines',
-    'version': "18.0.1.0.0",
-    'category': 'Moval General Addons',
-    'website': 'http://www.moval.es',
-    'author': 'Moval Agroingeniería',
-    'license': 'AGPL-3',
-    'depends': [
-        'account_payment_order',
+    "name": "Account Payment Order: Payment line date",
+    "summary": "Add payment request date to payment lines",
+    "version": "18.0.1.0.0",
+    "category": "Moval General Addons",
+    "website": "http://www.moval.es",
+    "author": "Moval Agroingeniería",
+    "license": "AGPL-3",
+    "depends": [
+        "account_payment_order",
     ],
-    'data': [
-        'views/account_payment_views.xml',
-        'views/account_payment_order.xml',
+    "data": [
+        "views/account_payment_views.xml",
+        "views/account_payment_order.xml",
     ],
-    'installable': True,
-    'application': False,
 }
