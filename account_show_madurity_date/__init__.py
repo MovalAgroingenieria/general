38e0f3d35da91279696d0046214b09c1ed9eb923
César Andrés Sánchez <rasec.andres@gmail.com>
2025-11-05 22:43:27 +0100

    [IMP] pylint account_show_madurity_date

--- account_show_madurity_date/__init__.py
+++ account_show_madurity_date/__init__.py
@@ -1,3 +1,2 @@
 # 2022 Moval Agroingeniería
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-
--- account_show_madurity_date/__manifest__.py
+++ account_show_madurity_date/__manifest__.py
@@ -2,19 +2,17 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'Account Show Madurity Date',
-    'summary': 'Show madurity date by default',
-    'version': "18.0.1.0.0",
-    'category': 'Moval General Addons',
-    'website': 'http://www.moval.es',
-    'author': 'Moval Agroingeniería',
-    'license': 'AGPL-3',
-    'installable': True,
-    'application': False,
-    'depends': [
-        'account',
+    "name": "Account Show Madurity Date",
+    "summary": "Show madurity date by default",
+    "version": "18.0.1.0.0",
+    "category": "Moval General Addons",
+    "website": "http://www.moval.es",
+    "author": "Moval Agroingeniería",
+    "license": "AGPL-3",
+    "depends": [
+        "account",
     ],
-    'data': [
-        'views/account_move_views.xml',
+    "data": [
+        "views/account_move_views.xml",
     ],
 }
