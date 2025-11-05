d800b253c433ab0c7cb51a60ab16846686fb162b
César Andrés Sánchez <rasec.andres@gmail.com>
2025-11-05 12:03:26 +0100

    [MIG] account_show_madurity_date: Migration to 18.0

--- account_show_madurity_date/__manifest__.py
+++ account_show_madurity_date/__manifest__.py
@@ -4,7 +4,7 @@
 {
     'name': 'Account Show Madurity Date',
     'summary': 'Show madurity date by default',
-    'version': '16.0.1.0.0',
+    'version': "18.0.1.0.0",
     'category': 'Moval General Addons',
     'website': 'http://www.moval.es',
     'author': 'Moval Agroingeniería',
