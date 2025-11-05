8fe014f39abd19af98ee6fe2042ac8a153d84464
César Andrés Sánchez <rasec.andres@gmail.com>
2025-11-05 12:03:26 +0100

    [MIG] display_decimal_precision: Migration to 18.0

--- display_decimal_precision/__manifest__.py
+++ display_decimal_precision/__manifest__.py
@@ -4,7 +4,7 @@
 {
     'name': 'Display Decimal Precision',
     'summary': 'Distinguish computation digits and display digits',
-    'version': '16.0.1.0.0',
+    'version': "18.0.1.0.0",
     'category': 'Hidden/Dependency',
     'website': 'http://www.moval.es',
     'author': 'Moval Agroingeniería',
