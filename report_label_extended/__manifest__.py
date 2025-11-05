aff7236177060c1dccc83a91725219b30e7137ac
César Andrés Sánchez <rasec.andres@gmail.com>
2025-11-05 12:08:10 +0100

    [MIG] report_label_extended: Migration to 18.0

---
+++ report_label_extended/__manifest__.py
@@ -0,0 +1,19 @@
+# 2020 Moval Agroingeniería
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Report Labels Extended",
+    "summary": "This module extends parent module functionality.",
+    "version": "18.0.1.0.0",
+    "category": "Moval General Addons",
+    "website": "http://www.moval.es",
+    "author": "Moval Agroingeniería",
+    "license": "AGPL-3",
+    "depends": [
+        "report_label",
+    ],
+    "data": [
+    ],
+    "installable": True,
+    "application": False,
+}
