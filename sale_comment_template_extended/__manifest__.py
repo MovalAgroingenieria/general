6b5ba8e0e72dd7873bea81a4a5f283e0595a6686
César Andrés Sánchez <rasec.andres@gmail.com>
2025-11-05 12:03:27 +0100

    [MIG] sale_comment_template_extended: Migration to 18.0

--- sale_comment_template_extended/__manifest__.py
+++ sale_comment_template_extended/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Sale Comments Extended",
     "summary": "Extends the functionality of the parent module",
-    "version": "16.0.0.0.1",
+    "version": "18.0.1.0.0",
     "license": "AGPL-3",
     "author": "Moval Agroingeniería",
     "website": "http://www.moval.es",
