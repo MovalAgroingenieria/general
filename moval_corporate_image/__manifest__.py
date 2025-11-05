e1abd71af41bda749f06c60e11cfcff27729a791
César Andrés Sánchez <rasec.andres@gmail.com>
2025-11-05 23:06:35 +0100

    [IMP] pylint moval_corporate_image

--- moval_corporate_image/__manifest__.py
+++ moval_corporate_image/__manifest__.py
@@ -2,25 +2,21 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'Moval Corporate Image',
-    'summary': 'Decorate instance with Moval logo',
-    'version': "18.0.1.0.0",
-    'category': 'Moval General Addons',
-    'website': 'http://www.moval.es',
-    'author': 'Moval Agroingeniería',
-    'license': 'AGPL-3',
-    'application': False,
-    'installable': True,
-    'depends': [
-        'web_responsive',
-        'fontawesome_ext',
+    "name": "Moval Corporate Image",
+    "summary": "Decorate instance with Moval logo",
+    "version": "18.0.1.0.0",
+    "category": "Moval General Addons",
+    "website": "http://www.moval.es",
+    "author": "Moval Agroingeniería",
+    "license": "AGPL-3",
+    "depends": [
+        "web_responsive",
+        "fontawesome_ext",
     ],
-    'data': [
-        'views/webclient_templates.xml'
-    ],
-    'assets': {
-        'web.assets_backend': [
-            'moval_corporate_image/static/src/components/apps_menu/apps_menu.xml',
+    "data": ["views/webclient_templates.xml"],
+    "assets": {
+        "web.assets_backend": [
+            "moval_corporate_image/static/src/components/apps_menu/apps_menu.xml",
         ],
     },
 }
