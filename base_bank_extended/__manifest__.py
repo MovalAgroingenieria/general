474b2035a915883db1e3c93644a1950f645c818b
César Andrés Sánchez <rasec.andres@gmail.com>
2025-11-05 12:03:26 +0100

    [MIG] base_bank_extended: Migration to 18.0

--- base_bank_extended/__manifest__.py
+++ base_bank_extended/__manifest__.py
@@ -4,7 +4,7 @@
 {
     'name': 'Resource Bank Extended',
     'summary': 'Extended functionality of res_bank base module',
-    'version': '16.0.1.0.0',
+    'version': "18.0.1.0.0",
     'author': 'Moval Agroingeniería',
     'license': 'AGPL-3',
     'category': 'Hidden',
