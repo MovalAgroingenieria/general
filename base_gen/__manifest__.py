a42d5c8a101a06fac998f80cebfa186f038a9182
César Andrés Sánchez <rasec.andres@gmail.com>
2025-11-05 12:06:00 +0100

    [MIG] base_gen: Migration to 18.0

---
+++ base_gen/__manifest__.py
@@ -0,0 +1,21 @@
+# 2022 Moval Agroingeniería
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+{
+    "name": "Base-General Module",
+    "summary": "General porpuse tools for any module",
+    "version": "18.0.1.0.0",
+    "author": "Moval Agroingeniería",
+    "license": "AGPL-3",
+    "category": "Hidden",
+    "depends": [
+        "base",
+    ],
+    'external_dependencies': {
+        'python': [
+            'Crypto.Cipher',
+        ],
+    },
+    "data": [],
+    "installable": True,
+    "application": False,
+}
---
+++ base_gen/models/__init__.py
@@ -0,0 +1,5 @@
+# 2021 Moval Agroingeniería
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+from . import simple_model
+from . import common_functions
