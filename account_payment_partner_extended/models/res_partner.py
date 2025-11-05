23d0e3e7f5c67bae2ca63be2666a15a66503c8ee
César Andrés Sánchez <rasec.andres@gmail.com>
2025-11-05 22:42:43 +0100

    account_payment_partner_extended

--- account_payment_partner_extended/models/res_partner.py
+++ account_payment_partner_extended/models/res_partner.py
@@ -1,7 +1,7 @@
 # 2023 Moval Agroingeniería
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
-from odoo import models, fields, api
+from odoo import api, fields, models
 
 
 class ResPartner(models.Model):
@@ -10,11 +10,13 @@ class ResPartner(models.Model):
     computed_customer_payment_mode_id = fields.Many2one(
         comodel_name="account.payment.mode",
         store=True,
-        compute="_compute_computed_customer_payment_mode_id",)
+        compute="_compute_computed_customer_payment_mode_id",
+    )
 
-    @api.depends('customer_payment_mode_id')
+    @api.depends("customer_payment_mode_id")
     def _compute_computed_customer_payment_mode_id(self):
         for record in self:
             if record.customer_payment_mode_id:
-                record.computed_customer_payment_mode_id = \
+                record.computed_customer_payment_mode_id = (
                     record.customer_payment_mode_id
+                )
