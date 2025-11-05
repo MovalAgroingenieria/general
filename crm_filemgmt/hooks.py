a2bee1b5edb81a44e5b88f79cc18900742991b8a
César Andrés Sánchez <rasec.andres@gmail.com>
2025-11-05 22:36:24 +0100

    [IMP] hooks: migrate post_init_hook to v18 and set crm_filemgmt prefix

--- crm_filemgmt/hooks.py
+++ crm_filemgmt/hooks.py
@@ -1,11 +1,15 @@
 # 2025 Moval Agroingeniería
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
-from odoo.api import Environment, SUPERUSER_ID
+from odoo import api
 
 
-def post_init_hook(cr, registry):
-    env = Environment(cr, SUPERUSER_ID, {})
-    # Parameter initialization
-    env['ir.config_parameter'].set_param(
-        'crm_filemgmt.annual_seq_prefix', 'EXP')
+def post_init_hook(env: api.Environment):
+    """
+    Odoo 18+ post-init hook.
+
+    Initializes the configuration parameter used by crm_filemgmt.
+    Using sudo() because ir.config_parameter writes are restricted.
+    """
+    icp = env["ir.config_parameter"].sudo()
+    icp.set_param("crm_filemgmt.annual_seq_prefix", "EXP")
