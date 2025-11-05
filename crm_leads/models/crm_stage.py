a981c2ee64a090f6a2556ba9c3f1aace708fe774
César Andrés Sánchez <rasec.andres@gmail.com>
2025-11-05 12:06:22 +0100

    [MIG] crm_leads: Migration to 18.0

---
+++ crm_leads/models/crm_stage.py
@@ -0,0 +1,14 @@
+# 2025 Moval Agroingeniería
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import api, fields, models
+
+
+class Stage(models.Model):
+    _inherit = "crm.stage"
+    
+    is_new = fields.Boolean(
+        string="Is New",
+        default=False,
+        help="Indicates if the stage is new.",
+    )
