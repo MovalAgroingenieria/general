15f0dab74e4cff70f078991445237fc58a78f1e4
César Andrés Sánchez <rasec.andres@gmail.com>
2025-11-10 18:19:58 +0100

    [ADD] display_decimal_precission test currency

--- display_decimal_precision/tests/__init__.py
+++ display_decimal_precision/tests/__init__.py
@@ -1,1 +1,5 @@
-from . import test_fields_monkeypatch, test_ir_http_currencies
+from . import (
+    test_fields_monkeypatch,
+    test_ir_http_currencies,
+    test_res_currency_display_rounding,
+)
