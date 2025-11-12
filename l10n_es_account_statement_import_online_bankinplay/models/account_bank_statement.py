65bd59fbac5a6767e6830a3d8d450bc9c86394b9
César Andrés Sánchez <rasec.andres@gmail.com>
2025-11-12 08:12:22 +0100

    [MIG] l10n_es_account_statement_import_online_bankinplay bank statement

--- l10n_es_account_statement_import_online_bankinplay/models/account_bank_statement.py
+++ l10n_es_account_statement_import_online_bankinplay/models/account_bank_statement.py
@@ -1,4 +1,4 @@
-# 2024 Moval Agroingeniería
+# 2024-2025 Moval Agroingeniería
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo import fields, models
@@ -7,27 +7,39 @@ from odoo import fields, models
 class AccountBankStatement(models.Model):
     _inherit = "account.bank.statement"
 
-    # TO be checked by later callbacks
+    # --- BankInPlay callback correlation fields -----------------------------
     bankinplay_signature = fields.Char(
-        string="Bankinplay signature",
+        help="Signature returned by Bankinplay to authenticate the callback.",
         readonly=True,
         copy=False,
+        index=True,
     )
 
     bankinplay_responseid = fields.Char(
-        string="Bankinplay response id",
+        help="Response identifier returned by Bankinplay for this request.",
         readonly=True,
         copy=False,
+        index=True,
     )
 
+    # --- Requested window used to build this statement ----------------------
     bankinplay_date_since = fields.Datetime(
-        string="Bankinplay date since id",
+        help="Start datetime used when requesting close movements.",
         readonly=True,
         copy=False,
     )
 
     bankinplay_date_until = fields.Datetime(
-        string="Bankinplay date until",
+        help="End datetime used when requesting close movements.",
         readonly=True,
         copy=False,
     )
+
+    _sql_constraints = [
+        # Prevent creating two statements for the same Bankinplay callback
+        (
+            "bankinplay_resp_sig_unique",
+            "unique(bankinplay_responseid, bankinplay_signature)",
+            "A statement for this Bankinplay response/signature already exists.",
+        ),
+    ]
