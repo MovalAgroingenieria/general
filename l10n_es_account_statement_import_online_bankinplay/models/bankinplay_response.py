aebbfbc316c6af3c3004ce57cc47c68026c2eb8b
César Andrés Sánchez <rasec.andres@gmail.com>
2025-11-12 08:23:43 +0100

    [MIG] l10n_es_account_statement_import_online_bankinplay bank statement

--- l10n_es_account_statement_import_online_bankinplay/models/bankinplay_response.py
+++ l10n_es_account_statement_import_online_bankinplay/models/bankinplay_response.py
@@ -1,45 +1,74 @@
-# 2024 Moval Agroingeniería
+# 2025 Moval Agroingeniería
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from datetime import datetime, timedelta
-
+from dateutil.relativedelta import relativedelta
 from odoo import api, fields, models
 
 
 class BankinplayResponse(models.Model):
     _name = "bankinplay.response"
     _description = (
-        "Intermediate table to store bankinplay responses when "
-        + "being used as remote endpoint"
+        "Intermediate table to temporarily store Bankinplay responses when "
+        "this database is acting as a remote endpoint."
     )
 
-    # TO be checked by later callbacks
+    # Used later by callbacks to route decrypted payloads correctly
     bankinplay_signature = fields.Char(
-        string="Bankinplay signature",
         readonly=True,
         copy=False,
+        index="btree_not_null",  # Changed from True to specific index type
+        help="Signature returned by Bankinplay to authenticate the callback.",
     )
 
     bankinplay_responseid = fields.Char(
-        string="Bankinplay response id",
+        string="Bankinplay Response ID",
         readonly=True,
         copy=False,
+        index="btree_not_null",  # Changed from True to specific index type
+        help="Response identifier returned by Bankinplay for this request.",
     )
 
-    bankinplay_response = fields.Char(
-        string="Bankinplay Response",
+    # Store (usually decrypted) JSON string for traceability/debugging
+    bankinplay_response = fields.Text(
         readonly=True,
         copy=False,
+        help="Full JSON payload that was forwarded to the client database.",
     )
 
+    # Where to forward the callback (base URL of the client DB)
     endpoint_return_url = fields.Char(
-        string="URL of client database",
+        string="Client Database URL",
         readonly=True,
         copy=False,
+        help="Base URL of the client DB that will receive "
+        "/webhook/bankinplay_callback.",
     )
+
+    _sql_constraints = [
+        (
+            "bankinplay_resp_sig_unique",
+            "unique(bankinplay_responseid, bankinplay_signature)",
+            "A stored response with this Bankinplay response/signature already exists.",
+        ),
+    ]
 
     @api.model
-    def delete_old_responses(self):
-        limit_date = datetime.now() - timedelta(weeks=1)
-        old_responses = self.search([("create_date", "<", limit_date)])
-        old_responses.unlink()
+    def delete_old_responses(self, weeks=1, batch_size=1000):
+        """Purge old transient rows to keep the table small.
+
+        Args:
+            weeks (int): Age threshold; rows older than this
+            are deleted. Default 1 week.
+            batch_size (int): Delete in batches to avoid long locks/PG bloat.
+        """
+        # Use Odoo's TZ-aware clock; create_date is UTC Datetime
+        limit_date = fields.Datetime.now() - relativedelta(weeks=weeks)
+
+        domain = [("create_date", "<", limit_date)]
+        while True:
+            old_batch = self.search(domain, limit=batch_size)
+            if not old_batch:
+                break
+            # sudo() in case the cron user has limited rights
+            old_batch.sudo().unlink()
+        return True
