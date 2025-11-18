d6c18e93478ed2bd423e0e4574ba74c378ee95ea
Juan Jose Bautista Gallego <jjbautista@moval.es>
2025-11-18 10:20:09 +0100

    [ADD] remotecontrol_agzoom: new remotecontrol agzoom added

---
+++ remotecontrol_agzoom/hooks.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# 2020 Moval Agroingeniería
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import api, SUPERUSER_ID
+
+
+def uninstall_hook(cr, registry):
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    env.ref('remotecontrol_agzoom.remotecontrol_agzoom').with_context(
+        force_unlink=True).unlink()
