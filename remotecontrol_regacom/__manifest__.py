be70ed4148423f7d2f3015ee1ab510e92a8b75e8
Salvador Sanchez Aroca <ssanchez@moval.es>
2025-11-18 13:10:20 +0100

    [NEW] remotecontrol_regacom: new integration module for SQL Server database.

---
+++ remotecontrol_regacom/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# 2025 Moval Agroingeniería
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "RemoteControl: Regacom",
+    "summary": "Remotecontrol Regacom (SQL) with default actions and "
+               "procedures for HIRU water meter readings",
+    "version": "10.0.1.0.0",
+    "category": "Tools",
+    "author": "Moval Agroingeniería",
+    "website": "http://www.moval.es",
+    "license": "AGPL-3",
+    "depends": [
+        "base_remotecontrol",
+    ],
+    "data": [
+        "data/data.xml",
+    ],
+    "uninstall_hook": "uninstall_hook",
+    "installable": True,
+    "application": False,
+}
