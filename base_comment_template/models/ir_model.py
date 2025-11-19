d5f26256ae9ba62c3e9233d0a586ceedf0710edf
César Andrés Sánchez <rasec.andres@gmail.com>
2025-11-19 12:49:28 +0100

    [MIG] base_comment_template migrate to v18

--- base_comment_template/models/ir_model.py
+++ base_comment_template/models/ir_model.py
@@ -10,24 +10,18 @@ class IrModel(models.Model):
     is_comment_template = fields.Boolean(
         string="Comment Template",
         default=False,
-        help="Whether this model supports in reports to add comment templates.",
+        help="Indicates whether this model supports comment templates in reports.",
     )
 
-    def _reflect_model_params(self, model):
-        vals = super()._reflect_model_params(model)
-        vals["is_comment_template"] = issubclass(
-            type(model), self.pool["comment.template"]
-        )
-        return vals
+    # -------------------------------------------------------------------------
+    # HELPERS
+    # -------------------------------------------------------------------------
 
     @api.model
-    def _instanciate(self, model_data):
-        model_class = super()._instanciate(model_data)
-        if (
-            model_data.get("is_comment_template")
-            and model_class._name != "comment.template"
-        ):
-            parents = model_class._inherit or []
-            parents = [parents] if isinstance(parents, str) else parents
-            model_class._inherit = parents + ["comment.template"]
-        return model_class
+    def get_comment_template_models(self):
+        """Return technical names of models that support comment templates.
+
+        This helper can be reused from other code instead of relying on
+        ORM internals or hard-coded model names.
+        """
+        return self.search([("is_comment_template", "=", True)]).mapped("model")
