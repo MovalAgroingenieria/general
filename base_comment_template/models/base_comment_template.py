f2a3b54373c2af0389a8953fda0b75c78390c0f8
César Andrés Sánchez <rasec.andres@gmail.com>
2025-11-19 12:48:53 +0100

    [MIG] base_comment_template migrate to v18

--- base_comment_template/models/base_comment_template.py
+++ base_comment_template/models/base_comment_template.py
@@ -3,46 +3,61 @@
 # Copyright 2020 NextERP Romania SRL
 # Copyright 2021-2022 Tecnativa - Víctor Martínez
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
 from odoo import _, api, fields, models
 from odoo.exceptions import ValidationError
 
 
 class BaseCommentTemplate(models.Model):
-    """Comment templates printed on reports"""
+    """Reusable comment templates printed on reports."""
 
     _name = "base.comment.template"
-    _description = "Comments Template"
+    _description = "Comment Template"
     _order = "sequence,id"
 
     active = fields.Boolean(default=True)
+
     position = fields.Selection(
         string="Position on document",
         selection=[("before_lines", "Top"), ("after_lines", "Bottom")],
         required=True,
         default="before_lines",
-        help="This field allows to select the position of the comment on reports.",
+        help="Select where this comment will be placed on the report.",
     )
+
     name = fields.Char(
         translate=True,
         required=True,
-        help="Name/description of this comment template",
+        help="Name/description of this comment template.",
     )
+
     text = fields.Html(
         string="Template",
         translate=True,
         required=True,
-        sanitize=False,
-        help="This is the text template that will be inserted into reports.",
+        sanitize=False,  # Set to True if you want HTML sanitization.
+        help="HTML text that will be inserted into reports.",
     )
+
     company_id = fields.Many2one(
         comodel_name="res.company",
         string="Company",
         ondelete="cascade",
         index=True,
-        help="If set, the comment template will be available only for the selected "
-        "company.",
+        help=(
+            "If set, the comment template will be available only for "
+            "the selected company."
+        ),
     )
-    global_template = fields.Boolean()
+
+    global_template = fields.Boolean(
+        help=(
+            "If enabled, this template is available for all partners that "
+            "match the model and domain, without being explicitly assigned "
+            "on the partner."
+        ),
+    )
+
     partner_ids = fields.Many2many(
         comodel_name="res.partner",
         relation="base_comment_template_res_partner_rel",
@@ -50,29 +65,46 @@ class BaseCommentTemplate(models.Model):
         column2="res_partner_id",
         string="Partner",
         readonly=True,
-        help="If set, the comment template will be available only for the selected "
-        "partner.",
+        help=(
+            "If set, the comment template will be available only for "
+            "the selected partners."
+        ),
     )
-    models = fields.Text(required=True)
+
+    # Comma-separated list of technical model names (e.g. "sale.order,account.move").
+    models = fields.Text(
+        required=True,
+        help="Comma-separated list of technical model names where this template is available.",
+    )
+
     model_ids = fields.Many2many(
         comodel_name="ir.model",
         compute="_compute_model_ids",
         compute_sudo=True,
-        string="IR Model",
-        help="This comment template will be available on this models. "
-        "You can see here only models allowed to set the coment template.",
+        string="Models",
+        help=(
+            "Models where this comment template is available. Only models "
+            "allowed to use comment templates are shown."
+        ),
         search="_search_model_ids",
     )
+
     domain = fields.Char(
         string="Filter Domain",
         required=True,
         default="[]",
-        help="This comment template will be available only for objects "
-        "that satisfy the condition",
+        help=(
+            "Domain (in Odoo domain syntax) that records must satisfy for this "
+            "template to be applicable."
+        ),
     )
+
     sequence = fields.Integer(
-        required=True, default=10, help="The smaller number = The higher priority"
+        required=True,
+        default=10,
+        help="Lower values give the template a higher priority.",
     )
+
     engine = fields.Selection(
         selection=[
             ("inline_template", "Inline Template"),
@@ -81,10 +113,15 @@ class BaseCommentTemplate(models.Model):
         ],
         required=True,
         default="inline_template",
-        help="This field allows to select the engine to use for rendering the template.",
+        help="Engine used to render the template.",
     )
 
+    # -------------------------------------------------------------------------
+    # HELPERS
+    # -------------------------------------------------------------------------
+
     def _get_ir_model_items(self, models):
+        """Return allowed ir.model records for the given technical model names."""
         return (
             self.env["ir.model"]
             .sudo()
@@ -97,38 +134,65 @@ class BaseCommentTemplate(models.Model):
             )
         )
 
+    # -------------------------------------------------------------------------
+    # COMPUTES & CONSTRAINTS
+    # -------------------------------------------------------------------------
+
     @api.depends("models")
     def _compute_model_ids(self):
-        im_model = self.env["ir.model"]
+        """Compute the ir.model records from the comma-separated 'models' field."""
         for item in self:
-            models = im_model.browse()
             if item.models:
-                models = self._get_ir_model_items(item.models.split(","))
+                model_names = [
+                    m.strip() for m in item.models.split(",") if m.strip()
+                ]
+                models = item._get_ir_model_items(model_names)
+            else:
+                models = self.env["ir.model"].browse()
             item.model_ids = [(6, 0, models.ids)]
 
     @api.constrains("models")
-    def check_models(self):
-        """Avoid non-existing or not allowed models (is_comment_template=True)"""
+    def _check_models(self):
+        """Ensure that all models exist and are allowed for comment templates."""
         for item in self.filtered("models"):
-            models = item.models.split(",")
-            res = self._get_ir_model_items(item.models.split(","))
-            if not res or len(res) != len(models):
+            model_names = [m.strip() for m in item.models.split(",") if m.strip()]
+            res = item._get_ir_model_items(model_names)
+            if not res or len(res) != len(model_names):
                 raise ValidationError(_("Some model (%s) not found") % item.models)
 
+    # -------------------------------------------------------------------------
+    # NAME GET
+    # -------------------------------------------------------------------------
+
+    @api.model
     def name_get(self):
-        """Redefine the name_get method to show the template name with the position."""
+        """Return template name with position (and optionally model names)."""
         res = []
+        selection_position = dict(self._fields["position"].selection)
         for item in self:
             name = "{} ({})".format(
-                item.name, dict(self._fields["position"].selection).get(item.position)
+                item.name,
+                selection_position.get(item.position),
             )
             if self.env.context.get("comment_template_model_display"):
                 name += " (%s)" % ", ".join(item.model_ids.mapped("name"))
             res.append((item.id, name))
         return res
 
+    # -------------------------------------------------------------------------
+    # SEARCH EXTENSION
+    # -------------------------------------------------------------------------
+
+    @api.model
     def _search_model_ids(self, operator, value):
-        # We cannot use model_ids.model in search() method to avoid access errors
+        """Custom search for model_ids, avoiding ir.model access right issues.
+
+        `operator` and `value` belong to the search API signature, but we only
+        use `value` as a substring filter on the technical model name.
+        """
+        if not value:
+            return []
+        # Use sudo to read all templates, then filter logically in Python.
         allowed_items = (
             self.sudo()
             .search([])
