8195f774186aacfc24ddbbdaafa23e40e48041c4
Git Moval <git@moval.es>
2025-11-25 11:17:00 +0100

    [MIG] hr_timesheet_sheet_autocreation: Migration to 17.0

--- hr_timesheet_sheet_autocreation/__manifest__.py
+++ hr_timesheet_sheet_autocreation/__manifest__.py
@@ -1,11 +1,10 @@
-# -*- coding: utf-8 -*-
 # 2025 Moval Agroingeniería
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
 {
     "name": "Autocreation of Timesheet Sheets",
     "summary": "Automatically create timesheet sheets for employees",
-    "version": "16.0.1.4.0",
+    "version": "17.0.1.0.0",
     "author": "Moval Agroingeniería",
     "maintainers": ["MovalAgroingenieria"],
     "website": "https://moval.es",
