2b959fec43628031b0c3908065de06b698f2017f
Git Moval <git@moval.es>
2025-11-25 11:17:00 +0100

    [MIG] mrp_production_deconstruction_ext: Migration to 17.0

--- mrp_production_deconstruction_ext/__manifest__.py
+++ mrp_production_deconstruction_ext/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "MRP Production Deconstruction",
-    'version': '16.0.1.0.0',
+    'version': "17.0.1.0.0",
     "author": "Avanzosc",
     "category": "MRP",
     "depends": [
