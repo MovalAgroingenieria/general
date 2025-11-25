92470d6f934761c4728890fe0c4b19fc4a21644a
Git Moval <git@moval.es>
2025-11-25 11:17:07 +0100

    [MIG] partner_address_street_number: Migration to 17.0

--- partner_address_street_number/__manifest__.py
+++ partner_address_street_number/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Partner Address Street Number",
     "summary": "This module add street number to partner address",
-    "version": "16.0.1.0.0",
+    "version": "17.0.1.0.0",
     "category": "Moval General Addons",
     "website": "http://www.moval.es",
     "author": "Moval Agroingeniería",
