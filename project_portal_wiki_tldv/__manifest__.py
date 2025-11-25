bad0b807372988032983c2be2d44091ddc0c93fa
Git Moval <git@moval.es>
2025-11-25 11:17:16 +0100

    [MIG] project_portal_wiki_tldv: Migration to 17.0

--- project_portal_wiki_tldv/__manifest__.py
+++ project_portal_wiki_tldv/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # 2025 Moval Agroingeniería
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
@@ -7,7 +6,7 @@
     "summary": "Adds a URL field in projects, displays Wiki pages on the "
                "portal, hides hours for portal users, and integrates TLDV "
                "video downloads into Wiki pages.",
-    "version": "16.0.1.0.0",
+    "version": "17.0.1.0.0",
     "category": "Moval General Addons",
     "website": "http://www.moval.es",
     "author": "Moval Agroingeniería",
