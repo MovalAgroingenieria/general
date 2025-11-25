b9c4395631c1ed6b227f1454918bed24eba7e91e
Git Moval <git@moval.es>
2025-11-25 11:17:00 +0100

    [MIG] stock_picking_cmr_report: Migration to 17.0

--- stock_picking_cmr_report/__manifest__.py
+++ stock_picking_cmr_report/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Stock Picking Cmr Report",
-    'version': '16.0.1.1.0',
+    'version': "17.0.1.0.0",
     "author": "Avanzosc",
     "category": "Inventory/Inventory",
     "website": "https://github.com/avanzosc/odoo-addons",
