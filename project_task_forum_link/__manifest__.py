52677b2eb56e5aa8490ecf66360fb20b334d26aa
Git Moval <git@moval.es>
2025-11-25 11:17:09 +0100

    [MIG] project_task_forum_link: Migration to 17.0

--- project_task_forum_link/__manifest__.py
+++ project_task_forum_link/__manifest__.py
@@ -1,11 +1,10 @@
-# -*- coding: utf-8 -*-
 # 2025 Moval Agroingeniería
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
 {
     "name": "Project Task Forum Link",
     "summary": "This module adds link between project tasks and forum posts.",
-    "version": '16.0.1.1.0',
+    "version": "17.0.1.0.0",
     "category": "Moval General Addons",
     "website": "http://www.moval.es",
     "author": "Moval Agroingeniería",
