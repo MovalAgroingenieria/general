6692388a4379481b945ff8efeba53d5f1315890e
Git Moval <git@moval.es>
2025-11-25 11:17:07 +0100

    [MIG] hr_attendance_with_lastnames: Migration to 17.0

--- hr_attendance_with_lastnames/__manifest__.py
+++ hr_attendance_with_lastnames/__manifest__.py
@@ -4,7 +4,7 @@
 {
     'name': 'Attendances Fix for lastnames',
     'summary': 'Add fields of lastnames to Employee to avoid errors',
-    'version': '16.0.1.0.0',
+    'version': "17.0.1.0.0",
     'author': 'Moval Agroingeniería',
     'license': 'AGPL-3',
     'category': 'Human Resources/Attendances',
