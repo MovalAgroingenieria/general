39a877024e9941a08234aa2f619ff949c6301c0a
Git Moval <git@moval.es>
2025-11-25 11:17:08 +0100

    [MIG] crm_registrymgmt_filemgmt: Migration to 17.0

--- crm_registrymgmt_filemgmt/__manifest__.py
+++ crm_registrymgmt_filemgmt/__manifest__.py
@@ -4,7 +4,7 @@
 {
     'name': 'Entry Registry and File Management',
     'summary': 'Tracking for entry registries and file management relation',
-    'version': '16.0.1.1.0',
+    'version': "17.0.1.0.0",
     'category': 'Customer Relationship Management',
     'website': 'https://odoo-community.org/',
     'author': 'Moval Agroingeniería',
