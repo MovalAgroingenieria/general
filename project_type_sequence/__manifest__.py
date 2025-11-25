783b8e4424aee632209a77545ab9f6be57136da6
Git Moval <git@moval.es>
2025-11-25 11:17:10 +0100

    [MIG] project_type_sequence: Migration to 17.0

--- project_type_sequence/__manifest__.py
+++ project_type_sequence/__manifest__.py
@@ -1,11 +1,10 @@
-# -*- coding: utf-8 -*-
 # 2025 Moval Agroingeniería
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
 {
     "name": "Project type sequence",
     "summary": "This module extends parent module functionality.",
-    "version": '16.0.1.1.0',
+    "version": "17.0.1.0.0",
     "category": "Moval General Addons",
     "website": "http://www.moval.es",
     "author": "Moval Agroingeniería",
