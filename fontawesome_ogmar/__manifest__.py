21f59f50e5841bd406974a96c70d3570e4e9ba22
Git Moval <git@moval.es>
2025-11-25 11:17:15 +0100

    [MIG] fontawesome_ogmar: Migration to 17.0

--- fontawesome_ogmar/__manifest__.py
+++ fontawesome_ogmar/__manifest__.py
@@ -4,7 +4,7 @@
 {
     'name': 'Fontawesome OGMAR',
     'summary': 'Add custom fontawesome fonts for OGMAR',
-    'version': '16.0.1.0.0',
+    'version': "17.0.1.0.0",
     'category': 'Moval General Addons',
     'website': 'http://www.moval.es',
     'author': 'Moval Agroingeniería',
