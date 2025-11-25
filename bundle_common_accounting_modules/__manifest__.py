4181d99559220f98d44301bba7e950ecd36355ba
Git Moval <git@moval.es>
2025-11-25 11:17:08 +0100

    [MIG] bundle_common_accounting_modules: Migration to 17.0

--- bundle_common_accounting_modules/__manifest__.py
+++ bundle_common_accounting_modules/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Bundle common accounting modules",
     "summary": "Install at once all common modules related to the accounting.",
-    "version": "16.0.1.0.0",
+    "version": "17.0.1.0.0",
     "author": "Moval Agroingeniería",
     "license": "AGPL-3",
     "category": "Hidden",
