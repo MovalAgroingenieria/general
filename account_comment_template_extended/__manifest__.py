d06b9daac04ff0ea9e9c226c3dbf210da35887ed
Git Moval <git@moval.es>
2025-11-25 11:17:14 +0100

    [MIG] account_comment_template_extended: Migration to 17.0

--- account_comment_template_extended/__manifest__.py
+++ account_comment_template_extended/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Account Comments Extended",
     "summary": "Extends the functionality of the parent module",
-    "version": "16.0.0.0.1",
+    "version": "17.0.1.0.0",
     "license": "AGPL-3",
     "author": "Moval Agroingeniería",
     "website": "http://www.moval.es",
