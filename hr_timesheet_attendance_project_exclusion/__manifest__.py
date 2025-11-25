bacaa930c4e6c68b33d2dc2ddbf0995437764703
Git Moval <git@moval.es>
2025-11-25 11:17:01 +0100

    [MIG] hr_timesheet_attendance_project_exclusion: Migration to 17.0

--- hr_timesheet_attendance_project_exclusion/__manifest__.py
+++ hr_timesheet_attendance_project_exclusion/__manifest__.py
@@ -1,11 +1,10 @@
-# -*- coding: utf-8 -*-
 # 2022 Moval Agroingeniería
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
 {
     "name": "Timesheet Attendances Project Exclusion",
     "summary": "Exclude the Internal Project in the hours count",
-    "version": "16.0.1.0.0",
+    "version": "17.0.1.0.0",
     "author": "Moval Agroingeniería",
     "license": "AGPL-3",
     "category": "Human Resources/Attendances",
