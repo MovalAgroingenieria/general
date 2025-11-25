f06e008452ac4256008cb1301d2a91c710f52fbb
Git Moval <git@moval.es>
2025-11-25 11:16:59 +0100

    [MIG] base_metering: Migration to 17.0

--- base_metering/__manifest__.py
+++ base_metering/__manifest__.py
@@ -4,7 +4,7 @@
 {
     'name': 'Base-Metering Module',
     'summary': 'General readings and consumptions data tools for any module',
-    'version': '16.0.1.0.0',
+    'version': "17.0.1.0.0",
     'author': 'Moval Agroingeniería',
     'license': 'AGPL-3',
     'category': 'Hidden',
