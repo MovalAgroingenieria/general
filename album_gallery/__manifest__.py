7658c1a4237243bcede194e7787e620bee278b05
Git Moval <git@moval.es>
2025-11-25 11:17:15 +0100

    [MIG] album_gallery: Migration to 17.0

--- album_gallery/__manifest__.py
+++ album_gallery/__manifest__.py
@@ -4,7 +4,7 @@
 {
     'name': 'Album Gallery',
     'summary': 'Organize photos and videos in albums in your webpages',
-    'version': '16.0.1.1.0',
+    'version': "17.0.1.0.0",
     'category': 'Moval General Addons',
     'website': 'http://www.moval.es',
     'author': 'Moval Agroingeniería',
