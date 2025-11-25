a855bad3035fc1cab55e8436a70a10224b9dbf79
Git Moval <git@moval.es>
2025-11-25 11:17:08 +0100

    [MIG] web_extended: Migration to 17.0

--- web_extended/__manifest__.py
+++ web_extended/__manifest__.py
@@ -1,11 +1,10 @@
-# -*- coding: utf-8 -*-
 # 2024 Moval Agroingeniería
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     'name': 'Web Extended',
     'summary': 'Extension of the core of the Odoo Web Client',
-    'version': '16.0.1.0.0',
+    'version': "17.0.1.0.0",
     'category': 'Moval General Addons',
     'website': 'http://www.moval.es',
     'author': 'Moval Agroingeniería',
