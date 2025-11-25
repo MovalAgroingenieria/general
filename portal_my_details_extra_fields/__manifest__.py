b839ce003e15cd925de7ef1c16881f72e1b0560e
Git Moval <git@moval.es>
2025-11-25 11:17:13 +0100

    [MIG] portal_my_details_extra_fields: Migration to 17.0

--- portal_my_details_extra_fields/__manifest__.py
+++ portal_my_details_extra_fields/__manifest__.py
@@ -1,11 +1,10 @@
-# -*- coding: utf-8 -*-
 # 2023 Moval Agroingeniería
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
 {
     'name': 'Portal Web Cliente My Details Add Extra Fields',
     'summary': 'Add some extra fields on Portal User My Details Web View',
-    'version': '16.0.1.0.0',
+    'version': "17.0.1.0.0",
     'category': 'Web',
     'website': 'https://www.moval.es',
     'author': 'Moval Agroingeniería',
