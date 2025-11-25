266432dc51f9d2134bb3e0c715a8ef0e14b1e280
Git Moval <git@moval.es>
2025-11-25 11:17:13 +0100

    [MIG] hr_employee_attendance_leave_report: Migration to 17.0

--- hr_employee_attendance_leave_report/__manifest__.py
+++ hr_employee_attendance_leave_report/__manifest__.py
@@ -1,11 +1,10 @@
-# -*- coding: utf-8 -*-
 # 2023 Moval Agroingeniería
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     'name': 'Employee Attendances / Leaves Report',
     'summary': 'Create a employee attendance/leaves report',
-    'version': '16.0.1.1.0',
+    'version': "17.0.1.0.0",
     'category': 'Moval General Addons',
     'website': 'http://www.moval.es',
     'author': 'Moval Agroingeniería',
