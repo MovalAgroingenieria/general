69915afe57e15e65ee0341e13541a2dc582f2483
Guillermo Amante <gamante@moval.es>
2025-11-26 14:23:55 +0100

    [NEW] remotecontrol_intages: new remotecontrol integration

---
+++ remotecontrol_intages/hooks.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# 2025 Moval Agroingeniería
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import api, SUPERUSER_ID
+
+def uninstall_hook(cr, registry):
+
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    remote = env.ref(
+        'remotecontrol_regacom.remotecontrol_regacom',
+        raise_if_not_found=False,
+    )
+    if remote:
+        remote.with_context(force_unlink=True).unlink()
