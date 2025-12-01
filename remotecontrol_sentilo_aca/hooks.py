da6b9f8d06928aa99fb0d663638bf5eacd12419e
Samuel Fernandez <sfernandez@moval.es>
2025-12-01 17:34:10 +0100

    [NEW] remotecontrol_sentilo_aca:  Added new module with instructions and configuration of the remotecontrol SENTILO ACA

---
+++ remotecontrol_sentilo_aca/hooks.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# 2025 Moval Agroingeniería
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import api, SUPERUSER_ID
+
+
+def uninstall_hook(cr, registry):
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    env.ref('remotecontrol_sentilo_aca.remotecontrol_sentilo_aca').with_context(
+        force_unlink=True).unlink()
