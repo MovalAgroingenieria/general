8d2a943e969406e2ddab57b4334902d91b0ad0ef
jmartinez <jmartinez@moval.es>
2025-12-12 12:58:43 +0100

    [UPD] hr_employee_public: modification on init method

--- hr_holidays_public_ext/models/hr_employee_public.py
+++ hr_holidays_public_ext/models/hr_employee_public.py
@@ -34,3 +34,26 @@ class HrEmployeePublic(models.Model):
         "not filled, employee creation date or the calendar start date "
         "will be used (the greatest of both)."
     )
+
+
+    def init(self):
+        """Force recreate hr_employee_public view with ALL hr_employee fields."""
+        # Get all stored fields from hr_employee model dynamically
+        employee_model = self.env['hr.employee']
+        fields_list = []
+
+        for name, field in employee_model._fields.items():
+            if field.store and field.type not in ['many2many', 'one2many']:
+                fields_list.append('emp.%s' % name)
+
+        field_select = ','.join(fields_list)
+
+        # Recreate the view with ALL available fields
+        tools.drop_view_if_exists(self.env.cr, 'hr_employee_public')
+        self.env.cr.execute("""CREATE or REPLACE VIEW hr_employee_public as (
+            SELECT %s
+            FROM hr_employee emp
+        )""" % field_select)
+
+        # Force registry reload to pick up new view structure
+        self.env.registry.clear_cache()
