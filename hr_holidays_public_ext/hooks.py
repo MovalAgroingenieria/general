a6ffc15dc125b8cd3da21fe184c4cbd9cb40a074
jmartinez <jmartinez@moval.es>
2025-12-12 16:21:14 +0100

    [UPD] hr_holidays_public_ext: refactor post-init hook and enhance hr_employee_public view field retrieval

---
+++ hr_holidays_public_ext/hooks.py
@@ -0,0 +1,9 @@
+# 2025 Moval Agroingeniería
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import api, SUPERUSER_ID
+
+
+def post_init_hook(cr, registry):
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    env['hr.employee.public'].init()
--- hr_holidays_public_ext/models/hr_employee.py
+++ hr_holidays_public_ext/models/hr_employee.py
@@ -1,37 +1,24 @@
 # 2025 Moval Agroingeniería
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import models, tools
+from odoo import models, api
 
 
 class HrEmployeePublic(models.Model):
     _inherit = 'hr.employee.public'
 
-    def _register_hook(self):
-        super()._register_hook()
-        self.init()
-
-    def init(self):
-        tools.drop_view_if_exists(self.env.cr, self._table)
-
-        # Get all stored fields from hr.employee
-        employee_fields = self.env['hr.employee']._fields
-        select_fields = []
-
-        for name, field in employee_fields.items():
-            if field.store and field.type not in ['one2many', 'many2many']:
-                select_fields.append(f'"{name}"')
-
-        fields_str = ', '.join(select_fields)
-
+    @api.model
+    def _get_fields(self):
+        """
+        Override to fetch fields directly from the database table.
+        This ensures that all columns present in hr_employee are included in the view,
+        regardless of whether they are defined in the hr.employee.public model or not.
+        """
         self.env.cr.execute("""
-            CREATE or REPLACE VIEW %s as (
-                SELECT
-                    %s
-                FROM
-                    hr_employee
-            )
-        """ % (self._table, fields_str))
-
-        # Clear cache to ensure new fields are recognized
-        self.env.registry.clear_cache()
+            SELECT column_name
+            FROM information_schema.columns
+            WHERE table_name = 'hr_employee'
+            ORDER BY ordinal_position
+        """)
+        columns = [row[0] for row in self.env.cr.fetchall()]
+        return ','.join('emp."%s"' % col for col in columns)
