f92f440c4069b9542adb4731a0986b36f48da2a0
jmartinez <jmartinez@moval.es>
2025-12-12 08:42:01 +0100

    [UPD] hr_holidays_public_ext: clean up whitespace in post-init hook function

--- hr_holidays_public_ext/__init__.py
+++ hr_holidays_public_ext/__init__.py
@@ -7,12 +7,12 @@ def post_init_hook(cr, registry):
     """Post-init hook to recreate hr_employee_public view with all fields."""
     from odoo import api, SUPERUSER_ID
     from odoo.tools import drop_view_if_exists
-    
+
     env = api.Environment(cr, SUPERUSER_ID, {})
-    
+
     # Get the hr_employee_public model
     model = env['hr.employee.public']
-    
+
     # Drop and recreate the view with all fields
     drop_view_if_exists(cr, model._table)
     cr.execute("""CREATE or REPLACE VIEW %s as (
