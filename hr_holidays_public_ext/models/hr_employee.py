30f2d3978c862945985570f0d1f9de64f9e1ca7c
jmartinez <jmartinez@moval.es>
2025-12-12 15:05:30 +0100

    [UPD] hr_holidays_public_ext: refactor hr_employee_public view initialization logic

--- hr_holidays_public_ext/models/hr_employee.py
+++ hr_holidays_public_ext/models/hr_employee.py
@@ -10,3 +10,28 @@ class HrEmployeePublic(models.Model):
     def _register_hook(self):
         super()._register_hook()
         self.init()
+
+    def init(self):
+        tools.drop_view_if_exists(self.env.cr, self._table)
+        
+        # Get all stored fields from hr.employee
+        employee_fields = self.env['hr.employee']._fields
+        select_fields = []
+        
+        for name, field in employee_fields.items():
+            if field.store and field.type not in ['one2many', 'many2many']:
+                select_fields.append(f'"{name}"')
+                
+        fields_str = ', '.join(select_fields)
+        
+        self.env.cr.execute("""
+            CREATE or REPLACE VIEW %s as (
+                SELECT
+                    %s
+                FROM
+                    hr_employee
+            )
+        """ % (self._table, fields_str))
+        
+        # Clear cache to ensure new fields are recognized
+        self.env.registry.clear_cache()
