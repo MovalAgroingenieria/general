451f28a293f485e1d2e0c704e1b6a2615b806be9
César Andrés Sánchez <rasec.andres@gmail.com>
2025-12-22 11:29:33 +0100

    [MIG] user_info_help_entries/: Migration to 18.0

--- user_info_help_entries/__manifest__.py
+++ user_info_help_entries/__manifest__.py
@@ -1,10 +1,9 @@
-# -*- coding: utf-8 -*-
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "User Info Help Entries",
     "summary": "Shortcut configuration for other services",
-    "version": "16.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Moval General Addons",
     "website": "http://www.moval.es",
     "author": "Moval Agroingeniería",
