6ac04fe394c04534fd5955f0c4833bbe51334639
César Andrés Sánchez <rasec.andres@gmail.com>
2026-01-01 10:08:41 +0100

    [MIG] base_report/: Migration to 18.0

--- base_report/__manifest__.py
+++ base_report/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Base Report",
     "summary": "Common modifications to all reports",
-    "version": "16.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Moval General Addons",
     "website": "https://www.moval.es",
     "author": "Moval Agroingeniería",
