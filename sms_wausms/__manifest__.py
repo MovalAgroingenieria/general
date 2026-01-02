3c68245fea66961b3d0804f57795bb87319f1374
César Andrés Sánchez <rasec.andres@gmail.com>
2026-01-02 05:23:56 +0100

    [MIG] sms_wausms/: Migration to 18.0

--- sms_wausms/__manifest__.py
+++ sms_wausms/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "SMS WauSMS",
     "summary": "SMS Text Messaging",
-    "version": "16.0.0.0.0",
+    "version": "18.0.1.0.0",
     "category": "Tools",
     "website": "https://www.moval.es",
     "author": "Moval Agroingeniería",
