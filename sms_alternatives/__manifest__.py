080402936eb2f34107d0ff94c620de7bb2e1c456
César Andrés Sánchez <rasec.andres@gmail.com>
2026-01-02 05:25:39 +0100

    [MIG] sms_alternatives/: Migration to 18.0

--- sms_alternatives/__manifest__.py
+++ sms_alternatives/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "SMS Alternatives",
     "summary": "Prepare settings for SMS alternatives",
-    "version": "16.0.0.0.0",
+    "version": "18.0.1.0.0",
     "category": "Hidden",
     "website": "https://www.moval.es",
     "author": "Moval Agroingeniería",
