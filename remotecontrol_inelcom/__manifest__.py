32ba53860968c056db666b2c2a49143c30305881
Guillermo Amante <gamante@moval.es>
2026-02-09 17:17:17 +0100

    [UPD] remotecontrol_inelcom: add new action

--- remotecontrol_inelcom/__manifest__.py
+++ remotecontrol_inelcom/__manifest__.py
@@ -6,7 +6,7 @@
     "name": "RemoteControl: Inelcom",
     "summary": "Remotecontrol Inelcom (REST) with default actions and "
                "procedures",
-    "version": "10.0.1.0.1",
+    "version": "10.0.1.0.2",
     "category": "Tools",
     "author": "Moval Agroingeniería",
     "website": "http://www.moval.es",
