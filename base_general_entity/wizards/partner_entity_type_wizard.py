8c29a52c3f62ff3f41f1df78820b4797dfbdc5b3
Salvador Sánchez <ssanchez@moval.es>
2026-03-13 13:16:10 +0100

    [UPD] base_general_entity: Added demo data, translations and test, aslo improve README ifle.

--- base_general_entity/wizards/partner_entity_type_wizard.py
+++ base_general_entity/wizards/partner_entity_type_wizard.py
@@ -2,6 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
 from odoo import fields, models
+from odoo.exceptions import ValidationError
 
 
 class PartnerEntityTypeWizard(models.TransientModel):
@@ -18,11 +19,40 @@ class PartnerEntityTypeWizard(models.TransientModel):
         required=True,
     )
 
+    def _validate_no_conflict(self, partners):
+        """Check that the action does not conflict with existing types."""
+        if self.action_type == "mark_primary":
+            conflicting = partners.filtered("is_secondary_entity")
+            if conflicting:
+                names = ", ".join(conflicting.mapped("name"))
+                raise ValidationError(
+                    self.env._(
+                        "The following contacts are already secondary "
+                        "members and cannot be marked as primary "
+                        "entities: %(names)s",
+                        names=names,
+                    )
+                )
+        elif self.action_type == "mark_secondary":
+            conflicting = partners.filtered("is_primary_entity")
+            if conflicting:
+                names = ", ".join(conflicting.mapped("name"))
+                raise ValidationError(
+                    self.env._(
+                        "The following contacts are already primary "
+                        "entities and cannot be marked as secondary "
+                        "members: %(names)s",
+                        names=names,
+                    )
+                )
+
     def action_apply(self):
         """Apply the selected action to the selected partners."""
         self.ensure_one()
         active_ids = self.env.context.get("active_ids", [])
         partners = self.env["res.partner"].browse(active_ids)
+
+        self._validate_no_conflict(partners)
 
         if self.action_type == "mark_primary":
             partners.write({"is_primary_entity": True})
