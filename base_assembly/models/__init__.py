1d437707b73f6e969e8483eca4b40e884b57966f
César Andrés Sánchez <candres@moval.es>
2026-03-30 16:02:48 +0200

    [FIX] base_assembly: restore missing model imports in models __init__

--- base_assembly/models/__init__.py
+++ base_assembly/models/__init__.py
@@ -3,14 +3,18 @@
 
 from . import assembly_mixin  # abstract mixins before models that _inherit them
 from . import (
-    assembly_agenda,
+    assembly_type,
     assembly_assembly,
+    assembly_agenda,
+    assembly_agenda_option,
     assembly_attendee,
     assembly_attendee_vote,
     assembly_delegation,
-    assembly_type,
+    assembly_representation,
     assembly_voting,
     assembly_voting_line,
     assembly_voting_result,
+    assembly_document_preview_wizard,
+    mail_compose_message,
     res_partner,
 )
