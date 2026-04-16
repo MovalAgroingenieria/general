6f0b99fba4f4f81bfe262052ba90bae2dc787058
César Andrés Sánchez <candres@moval.es>
2026-04-16 12:25:05 +0200

    [MIG] bundle_user_experience_modules : migration to V18

---
+++ bundle_user_experience_modules/__manifest__.py
@@ -0,0 +1,36 @@
+# 2026 Moval Agroingeniería
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+{
+    "name": "Bundle user experience modules",
+    "summary": "Install at once all common modules related to the user experience",
+    "version": "18.0.1.0.0",
+    "author": "Moval Agroingeniería",
+    "license": "AGPL-3",
+    "category": "Hidden",
+    "depends": [
+        "base_technical_user",
+        "base_fontawesome",
+        "disable_odoo_online",
+        "moval_corporate_image",
+        "partner_address_street_type",
+        "l10n_es_partner_address_street_type",
+        "partner_address_street_number",
+        "report_xml",
+        "web_company_color",
+        "web_copy_confirm",
+        "web_dark_mode",
+        "web_group_expand",
+        "web_chatter_position",
+        "web_no_bubble",
+        "web_pivot_computed_measure",
+        "web_refresher",
+        "web_remember_tree_column_width",
+        "web_responsive",
+        "web_send_message_popup",
+        "web_sheet_full_width",
+        "web_tree_dynamic_colored_field",
+        "web_widget_one2many_tree_line_duplicate",
+        # "web_disable_export_group", # Install later all
+    ],
+}
