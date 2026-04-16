296487621613f807d822d9b89c217a83a756a561
César Andrés Sánchez <candres@moval.es>
2026-04-16 12:23:20 +0200

    [MIG] bundle_advanced_accounting_modules: migration to V18

---
+++ bundle_advanced_accounting_modules/__manifest__.py
@@ -0,0 +1,18 @@
+# 2026 Moval Agroingeniería
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+{
+    "name": "Bundle advanced accounting modules",
+    "summary": "Install at once all modules related to the accounting.",
+    "version": "18.0.1.0.0",
+    "author": "Moval Agroingeniería",
+    "license": "AGPL-3",
+    "category": "Hidden",
+    "depends": [
+        "bundle_common_accounting_modules",
+        "analytic",
+        "mis_builder",
+        "mis_builder_budget",
+        "payment",
+    ],
+}
