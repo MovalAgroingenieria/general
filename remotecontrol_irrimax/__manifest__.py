74112ab688792047141efd38a7121787669dcd58
baty7 <jjbautista@moval.es>
2026-07-21 14:09:32 +0200

    [ADD] remotecontrol_irrimax: add remotecontrol module

---
+++ remotecontrol_irrimax/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# 2026 Moval Agroingeniería
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "RemoteControl: Irrimax",
+    "summary": "Remotecontrol Irrimax (REST) with default actions and "
+               "procedures",
+    "version": "10.0.1.0.0",
+    "category": "Tools",
+    "author": "Moval Agroingeniería",
+    "website": "http://www.moval.es",
+    "license": "AGPL-3",
+    "depends": [
+        "base_remotecontrol",
+        "mdm_sensor_management_remotecontrol",
+    ],
+    "data": [
+        "data/data.xml",
+    ],
+    "uninstall_hook": "uninstall_hook",
+    "installable": True,
+    "application": False,
+}
