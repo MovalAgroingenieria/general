fc99ae0fe20f3cfd0b228693de7ca413fe02a7a4
gamante <gamante@moval.es>
2026-09-08 12:53:10 +0200

    [NEW] remotecontrol_kuvio: new remotecontrol module

---
+++ remotecontrol_kuvio/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# 2026 Moval Agroingeniería
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Kuvio Remote Control Integration",
+    "summary": "Remote monitoring and control integration with Kuvio "
+               "telemetry platform",
+    "version": "10.0.1.0.0",
+    "category": "Tools",
+    "website": "http://www.moval.es",
+    "author": "Moval Agroingeniería",
+    "license": "AGPL-3",
+    "depends": [
+        "base_remotecontrol",
+    ],
+    "data": [
+        "data/data.xml",
+    ],
+    "installable": True,
+}
